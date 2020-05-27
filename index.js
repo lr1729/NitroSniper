90c1f57af381e74afe7b78c9bb8bea972f4885ad
ling1729 <renlingfeng21@gmail.com>
2020-05-26 21:48:58 -0700

    test

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const Discord = require("discord.js");
 const axios = require('axios').default;
+const request = require('request');
 
 const client = new Discord.Client();
 const {
     account_token
 } = require("./config.json");
 
-
+client.login(account_token)
 
 client.on('ready', () => {
     console.log(`Looking for discord gifts...`);
+    setInterval(check, 1000);
 });
 
 client.on('message', message => {
@@ -21,19 +23,51 @@ client.on('message', message => {
         var NitroCode = NitroUrl[0].split('/')[1];
 
         console.log(`Nitro found in ${message.guild.name}`);
-        
-        axios({
-            method: 'POST',
-            url: `https://discordapp.com/api/v6/entitlements/gift-codes/${NitroCode}/redeem`, 
-            headers: 
-            {
-            'Authorization': client.account_token 
-            }
-        }).then(
-            () => console.log(`Successfull redeemed on ${message.guild.name}`)
-        ).catch(ex => console.log(`Error | Failed to claim Nitro`))
+
+        checkCode(NitroCode, account_token);
     }
 })
 
-client.login(account_token)
+async function check() {
+    NitroCode = randgen(16);
+    checkCode(NitroCode, account_token);
+}
+
+
+async function checkCode(code, token) {
+    const options = {
+        url: 'https://discordapp.com/api/v6/entitlements/gift-codes/'+code+'/redeem',
+        method: "POST",
+        headers: {
+            "Authorization": token,
+        }
+    };
+    request(options, function(err, res, body) {
+        if(/This gift has been redeemed already/.test(body)){
+            console.log("\x1b[33m", "Code already redeemed: " + code);
+        }
+        if(/nitro/.test(body)){
+            console.log("\x1b[33m", "Redeemed code: " + code);
+        }
+        if(/Unknown Gift Code/.test(body)){
+            console.log("\x1b[33m", "Invalid Gift Code: " + code);
+        }
+        console.log(body);
+    });
+}
+
+function randgen(length){
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    let chanceNum = 10;
+    for(let i = 0; i < length; i++){
+        if(Math.random() < 0.1)
+            result += Math.floor(Math.random() * 10);
+        else
+            result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+
 
